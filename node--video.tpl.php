561aee68be57b32b7b9e600d5a343da88bda9d95
Jhonatan <Jhonatan_M@web-1.laopinion>
2016-06-22 10:31:17 -0500

    actualizando el tpl de video

--- node--video.tpl.php
+++ node--video.tpl.php
@@ -71,10 +71,8 @@
         <!--ANUNCIO 300x250--><div class="pauta p300x250"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',53); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN 3.ANUNCIO 300x250-->
         <!--Juegos--><div class="juegos"><a href="http://juegos.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/juegos.jpg"></a></div><!--FIN Juegos-->
       </div><!--FIN Derecha-->
-<<<<<<< HEAD
 </div>
 <!--Fin Contenido-->
-=======
       <!--Noticias recientes footer-->
       <div class="recientes_footer">
         <div class="titulo"><div class="color"></div><h2>ULTIMAS NOTICIAS</h2></div>
@@ -96,7 +94,7 @@
         <div class="productos"><div class="color"></div><a href="<?php print base_path(); ?>productos">Productos La Opinión</a></div><?php include_once("productos.php") ?>
       </div><!--Fin Productos la opinion-->
 </div><!--Fin Video-->
->>>>>>> 2df024463ca80990d82ad415c4d11cb43d4efd0b
+
 <!-- Script FACEBOOK player -->
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -124,7 +122,6 @@ jQuery (function($){
     var facebookplayer = "<div class=fb-video data-href=https://www.facebook.com/" + id + "/ data-width=1006 data-show-text=false><blockquote cite=https://www.facebook.com/" + id + "/ class=fb-xfbml-parse-ignore><a href=https://www.facebook.com/" + id + "/></a></blockquote></div>";
     var vineplayer = "<iframe src=https://vine.co/v/" + id + "/embed/simple width=1006 height=500 frameborder=0></iframe>";
     var instagramplayer = "<blockquote class=instagram-media data-instgrm-captioned data-instgrm-version=7 style= background:#FFF; border:0; border-radius:3px; box-shadow:0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15); margin: 1px; max-width:658px; padding:0; width:99.375%; width:-webkit-calc(100% - 2px); width:calc(100% - 2px);><div style=padding:8px;> <div style= background:#F8F8F8; line-height:0; margin-top:40px; padding:28.125% 0; text-align:center; width:100%;> <div style= background:url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACwAAAAsCAMAAAApWqozAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAMUExURczMzPf399fX1+bm5mzY9AMAAADiSURBVDjLvZXbEsMgCES5/P8/t9FuRVCRmU73JWlzosgSIIZURCjo/ad+EQJJB4Hv8BFt+IDpQoCx1wjOSBFhh2XssxEIYn3ulI/6MNReE07UIWJEv8UEOWDS88LY97kqyTliJKKtuYBbruAyVh5wOHiXmpi5we58Ek028czwyuQdLKPG1Bkb4NnM+VeAnfHqn1k4+GPT6uGQcvu2h2OVuIf/gWUFyy8OWEpdyZSa3aVCqpVoVvzZZ2VTnn2wU8qzVjDDetO90GSy9mVLqtgYSy231MxrY6I2gGqjrTY0L8fxCxfCBbhWrsYYAAAAAElFTkSuQmCC); display:block; height:44px; margin:0 auto -44px; position:relative; top:-22px; width:44px;></div></div> <p style= margin:8px 0 0 0; padding:0 4px;> <a href=https://www.instagram.com/p/" + id + "/ style= color:#000; font-family:Arial,sans-serif; font-size:14px; font-style:normal; font-weight:normal; line-height:17px; text-decoration:none; word-wrap:break-word; target=_blank>Te enseñamos a preparar las tajadas de maduro de otra manera. Mira la receta completa en nuestro portal.</a></p> <p style= color:#c9c8cd; font-family:Arial,sans-serif; font-size:14px; line-height:17px; margin-bottom:0; margin-top:8px; overflow:hidden; padding:8px 0 7px; text-align:center; text-overflow:ellipsis; white-space:nowrap;>Un vídeo publicado por Deléitese Gastronomia (@deleitese_co) el <time style= font-family:Arial,sans-serif; font-size:14px; line-height:17px; datetime=2016-06-19T20:43:02+00:00>19 de Jun de 2016 a la(s) 1:43 PDT</time></p></div></blockquote>";
-<<<<<<< HEAD
 
 
     if(plataforma == "YouTube"){
@@ -138,7 +135,6 @@ jQuery (function($){
     }else
     if(plataforma == "Instagram"){
       $('.video-player').append(instagramplayer);
-=======
     //var instagramplayer = "<blockquote class=instagram-media data-instgrm-captioned data-instgrm-version=7 <div style=width:100%;> </div> <p<a href=https://www.instagram.com/p/" + id + "/ </p></blockquote>";
 
 
@@ -153,14 +149,7 @@ jQuery (function($){
     }else
     if(plataforma == "Instagram"){
       $('#video .contenido .video-player').append(instagramplayer);
->>>>>>> 2df024463ca80990d82ad415c4d11cb43d4efd0b
     }
   });
 });
 </script>
-<<<<<<< HEAD
-<!-- FIN Script player -->
-=======
-<!-- FIN Script player -->
-<!--Fin Contenido-->
->>>>>>> 2df024463ca80990d82ad415c4d11cb43d4efd0b
