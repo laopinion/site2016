621249089de3636c5fa9a0302c8a219fc3b4a061
John Andrey Serrano <andrey_201015@hotmail.com>
2016-06-29 18:01:07 -0500

    Realice algunas modificaciones  en el archivos de sass

--- node--video.tpl.php
+++ node--video.tpl.php
@@ -1,5 +1,6 @@
 <!--Contenido-->
 <div id="video">
+  <!--ANUNCIO 1000x30--><div class="pauta p1000x30"><?php $block =block_load('block',57); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div><!--FIN ANUNCIO 1000x30-->
     <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); print drupal_render(_block_get_renderable_array(_block_render_blocks(array($taxonomy_menu_block))));?><!--FIN seccion-->
        <?php include("compartir.tpl.php"); ?>
        <?php include("recomendada.tpl.php"); ?>
