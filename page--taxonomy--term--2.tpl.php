46e0c4c8eb7b73b5d91f96f2c48dd852debaad5d
John Andrey Serrano <andrey_201015@hotmail.com>
2016-07-02 12:18:13 -0500

    Modificando detalles de las secciones

--- page--taxonomy--term--2.tpl.php
+++ page--taxonomy--term--2.tpl.php
@@ -12,15 +12,16 @@
           <!--ANUNCIO 300x250--><div class="pauta p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 300x250-->
         </div>
         <div class="mid">           
-          <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_3'); ?><!--FIN Centro-->
-          <!--ANUNCIO 728x90--><div class="pauta p728x90" style="margin:0px 135px 15px 135px"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 728x90-->
+          <!--Por los barrios--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_2'); ?><!--FIN Por los barrios-->
         </div>
-        <div class="mid"><!--Seguir leyendo-->          
+        <!--ANUNCIO 728x90--><div class="pauta p728x90" style="margin:0px 135px 15px 135px"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 728x90-->
+        <div class="mid"><!--Seguir leyendo-->
+          <div class="linea"></div>         
           <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_5'); ?><!--FIN Centro-->
           <div class="divisor"></div>
-          <!--ANUNCIO 300x250--><div class="pauta pauta_lateral p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 300x250-->
+          <!--ANUNCIO 300x250--><div class="pauta pauta_lateral p300x250" style="margin-top: 35px;float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 300x250-->
         </div><!--Fin Seguir leyendo--> 
-        <div class="mid"> <!--Mas noticias-->        
+        <div class="mid" style="border: none;"> <!--Mas noticias-->        
           <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_4'); ?><!--FIN Centro-->
         </div><!--Fin Mas noticias-->
       </div>  
--- page--taxonomy--term--7.tpl.php
+++ page--taxonomy--term--7.tpl.php
@@ -13,17 +13,17 @@
         </div> <!--Fin der-->
         <div class="mid">           
           <!--Pamplona--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_3'); ?><!--FIN Pamplona-->
-          <!--ANUNCIO 728x90--><div class="pauta p728x90" style="margin:0px 135px 15px 135px"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 728x90-->
         </div>
+        <!--ANUNCIO 728x90--><div class="pauta p728x90" style="margin: 0 135px 15px 135px;"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 728x90-->
          <div class="mid">           
           <!--Ocaña--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_1'); ?><!--FIN Ocaña-->
         </div>
         <div class="mid"><!--Seguir leyendo-->          
           <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_5'); ?><!--FIN Centro-->
           <div class="divisor"></div>
-          <!--ANUNCIO 300x250--><div class="pauta pauta_lateral p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 300x250-->
+          <!--ANUNCIO 300x250--><div class="pauta p300x250" style="margin-top: 35px;float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php $block =block_load('block',49); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div></div><!--FIN ANUNCIO 300x250-->
         </div><!--Fin Seguir leyendo--> 
-        <div class="mid"><!--Mas noticias-->            
+        <div class="mid" style="border: none;"><!--Mas noticias-->            
           <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_4'); ?><!--FIN Centro-->
         </div><!--Mas noticias--> 
       </div><!-- Fin content--> 
