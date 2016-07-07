3481ebd57b53c4fcb6c90b06dc6ac718f247e88e
John Andrey Serrano <andrey_201015@hotmail.com>
2016-07-07 16:12:02 -0500

    Tpl tags

---
+++ page--taxonomy--tags.tpl.php
@@ -0,0 +1,5 @@
+<section id="actualidad">
+      <div class="content">
+       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat eius pariatur explicabo quaerat vero sunt corrupti nam consectetur assumenda optio. Nesciunt quidem recusandae alias quia enim, atque fugiat aliquam veritatis?
+      </div><!-- Fin content--> 
+</section> <!-- /#seccion -->
