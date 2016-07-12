e4e35679a8976cc0b868e90d962d675ab2ce35a4
John Andrey Serrano <andrey_201015@hotmail.com>
2016-07-12 18:07:16 -0500

    MOdificaciones al tpl de suscripciones

---
+++ node--90555.tpl.php
@@ -0,0 +1,63 @@
+<section id="suscripciones">
+  <div class="content">
+    <section class="titulo"><h2>Suscripciones</h2></section>
+    <section class="logos">
+      <div class="izq">
+        <img src="<?php print base_path(); ?>sites/default/themes/op/images/suscripcion_izq.jpg" alt="Suscripción" width="420">
+      </div>
+      <div class="der">
+        <div class="logo_1"></div>
+        <div class="logo_2"></div>
+        <div class="logo_3"></div>
+        <article>
+          <p>Por ser suscriptor se le obsequia una publicación de una foto social en grupo para el día de sus cumpleaños y además obtiene un 15% de descuento en avisos sociales.</p>
+          <p>En optativos, se les da un descuento dependiendo de la promoción; y eventos se obsequian boletas o se hacen descuentos especiales.</p>
+          <p>Le llegara el servicio del periódico, adicional con todos los fascículos e insertos antes de las 6am a su casa u oficina.</p>
+        </article>
+      </div>
+    </section>
+    <section class="meses">
+      <div class="info">
+        
+        <div class="tiempo">
+          <h2>1</h2>
+          <div class="descripcion">
+            <span><strong><aside>Año</aside>$299.000</strong> <br><p>A pagar por adquirir la suscripción</p></span>
+          </div>
+        </div>
+        <span class="diario">$828 Ejemplar diario con la suscripción</span>
+      </div>
+      <div class="info">
+        <div class="tiempo">
+          <h2>6</h2>
+          <div class="descripcion">
+            <span><strong><aside>Meses</aside>$190.000</strong> <br><p>A pagar por adquirir la suscripción</p></span>
+          </div>
+        </div>
+        <span class="diario">$1.056 Ejemplar diario con la suscripción</span>
+      </div>
+      <div class="info"> 
+        <div class="tiempo">
+          <h2>3</h2>
+          <div class="descripcion">
+            <span><strong><aside>Meses</aside>$110.000</strong> <br><p>A pagar por adquirir la suscripción</p></span>
+          </div>
+        </div>
+        <span class="diario">$1.222 Ejemplar diario con la suscripción</span>
+      </div>
+      <div class="info">
+        <div class="tiempo">
+           <h2>1</h2>
+          <div class="descripcion">
+            <span><strong><aside>Meses</aside>$42.000</strong> <br><p>A pagar por adquirir la suscripción</p></span>
+          </div>
+        </div>      
+        <span class="diario">$1.400 Ejemplar diario con la suscripción</span>
+      </div>
+      <div class="info_ps">
+        <p>Valor diaria del periódico: De Lunes a Sábados:$1.500 y los Domingos:$2.800.</p>
+      </div>
+    </section>
+  </div><!-- Fin content--> 
+</section> <!-- /#seccion -->
+     
--- webform-form-90555.tpl.php
+++
@@ -1,6 +0,0 @@
-<section id="suscripciones">
-  <div class="content">
-    
-  </div><!-- Fin content--> 
-</section> <!-- /#seccion -->
-     
