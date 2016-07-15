054fbce9cc3ac9b6a598abd2e002c6cc1fb08f19
John Andrey Serrano <andrey_201015@hotmail.com>
2016-07-15 18:05:25 -0500

    Terminada la parte de contactenos y finalizando la seccion de participacion

---
+++ page--taxonomy--term--20.tpl.php
@@ -0,0 +1,18 @@
+<section id="participacion">
+      <div class="content">
+        <?php if (!empty($tabs['#primary'])): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+        <!--ANUNCIO 1000x30--><div class="p1000x30"><?php // $block =block_load('block',39); $output = drupal_render(_block_get_renderable_array(_block_render_blocks(array($block)))); print $output; ?></div><!--FIN 1.ANUNCIO 1000x30-->
+        <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); print drupal_render(_block_get_renderable_array(_block_render_blocks(array($taxonomy_menu_block))));?><!--FIN seccion-->
+        <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_3'); ?><!--FIN Centro-->
+        <!--Multimedia--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_21'); ?><!--FIN Multimedia-->
+        <div class="izq">
+         <!--denuncia--><?php $block = module_invoke('webform', 'block_view', 'client-block-134');print render($block['content']); ?><!--FIN denuncia-->
+        </div>
+        <div class="der">
+          <!--Edicion impresa--><?php $view = views_get_view('sidebar'); print $view->preview('block_4'); ?><!--FIN Edicion impresa-->
+        <!--Suscribcion--><div class="suscripcion"><a href="<?php print base_path(); ?>suscripciones"><img src="<?php print base_path(); ?>sites/default/themes/op/images/suscripcion2.jpg"></a></div><!--FIN Suscribcion-->
+        <!--Clasificados--><div class="clasificados"><a href="http://clasificados.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/clasificados.jpg"></a></div><!--FIN Clasificados-->
+           <!--Juegos--><div class="juegos"><a href="http://juegos.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/juegos.jpg"></a></div><!--FIN Juegos-->
+        </div>
+      </div><!-- Fin content--> 
+</section> <!-- /#seccion -->
--- webform-form-90556.tpl.php
+++ webform-form-90556.tpl.php
@@ -1,44 +1,42 @@
-
-<style>
-.links{display: none;}
-</style>
-      <?php if (!empty($tabs['#primary'])): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-      
-        <!--<div class="content"><?php // print render($page['content']); ?></div> -->
-
-        <div id="contactenos">
-          <h2>Contáctenos</h2>
-          <div class="arriba">
-            <div class="leyenda">Contáctenos para dudas, sugerencias, problemas, comentarios, o simplemente para comunicarse con el equipo de Laopinión.com.co.</div>
-            <div class="info"> <!-- INFO -->
-              <div class="item item1">
-                <div class="logo_o"></div>
-                <div class="dir">Av. 4 Nro. 16 - 12 La Playa</div>
-                <div class="ciudad">Cúcuta - Colombia</div>
-              </div>
-              <div class="item item2">
-                <div class="tel"></div>
-                <div class="pbx">Teléfono (+57) (7) 5829999</div>
-                <div class="fax">Fax (+57) (7) 5831502</div>
-              </div>
-              <div class="item item3">
-                <ul class="redes-sociales">
-                    <li class="facebook"><a href="https://www.facebook.com/laopinioncucuta" target="_blank" style="border:0px;">Facebook</a></li>
-                    <li class="twitter"><a href="https://www.twitter.com/opinionweb" target="_blank" style="border:0px;">Twitter</a></li>
-                    <li class="instagram"><a href="https://www.instagram.com/laopinioncucuta" target="_blank" style="border:0px;">Instagram</a></li>
-                    <li class="youtube"><a href="https://www.youtube.com/user/WEBLAOPINION" target="_blank" style="border:0px;">Youtube</a></li>
-                </ul>
-              </div>
-            </div> <!-- INFO -->
-          </div> 
-          <div class="abajo">
-            <h3>Escríbanos</h3>
-              <div class="logo_o"></div>
-              <div class="formulario">
-                  <?php
-                  print drupal_render($form['submitted']);
-                  print drupal_render_children($form);
-                  ?> 
-              </div>    
+<section id="contactenos">
+      <div class="content">
+        <div class="izq">
+          <h1>Contáctenos</h1>
+          <div class="gps">
+            <div class="texto">
+              <span>Diario La Opinión</span>
+              Av. 4 Nro. 16 - 12 La Playa
+              Cúcuta - Colombia
+            </div>
           </div>
-        </div>          
+          <div class="tel">
+            <div class="texto">
+              <span>Teléfonos</span>
+              (+57) 7 582.9999 <br>
+              Fax (+57) 7 583.1502
+            </div>
+          </div>
+          <div class="siganos">
+            <h2>Siganos</h2>
+            <div class="f"></div>laopinioncucuta
+            <div class="t"></div>@laopinioncucuta
+            <div class="i"></div>@laopinioncucuta
+            <div class="y"></div>laopinioncucuta
+          </div>
+          <div class="formulario">
+            <h2>Escríbanos</h2>
+            <?php
+              print drupal_render($form['submitted']);
+              print drupal_render_children($form);
+            ?>
+          </div>
+        </div>
+        <div class="der">
+        <!--Edicion impresa--><?php $view = views_get_view('sidebar'); print $view->preview('block_4'); ?><!--FIN Edicion impresa-->
+        <!--Calendario tributario--><?php $view = views_get_view('sidebar'); print $view->preview('block_6'); ?><!--FIN Calendario triubtario-->
+        <!--Suscribcion--><div class="suscripcion"><a href="<?php print base_path(); ?>suscripciones"><img src="<?php print base_path(); ?>sites/default/themes/op/images/suscripcion2.jpg"></a></div><!--FIN Suscribcion-->
+        <!--Clasificados--><div class="clasificados"><a href="http://clasificados.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/clasificados.jpg"></a></div><!--FIN Clasificados-->
+           <!--Juegos--><div class="juegos"><a href="http://juegos.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/juegos.jpg"></a></div><!--FIN Juegos-->
+        </div>         
+      </div><!-- Fin content--> 
+</section> <!-- /#seccion -->
