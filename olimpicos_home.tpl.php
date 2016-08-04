e9b54d6dfb897a3c188465d69b3f4d4971e19b1f
John Andrey Serrano <andrey_201015@hotmail.com>
2016-08-04 16:47:25 -0500

    Terminando pagina de error 404

---
+++ olimpicos_home.tpl.php
@@ -0,0 +1,11 @@
+<!--COPA AMERICA-->
+<seccion id="olimpicos_home">
+  <div class="cintillo"></div>
+  <div class="contenido">
+  <!--Destacada--><?php //$view = views_get_view('copa_america'); print $view->preview('block_4'); ?><!--FIN Destacada-->
+    <div class="dos">
+        <!--dos notas--><?php //$view = views_get_view('copa_america'); print $view->preview('block_5'); ?><!--FIN dos notas-->
+    </div>       
+  </div>    
+</seccion>
+<!--FIN COPA AMERICA-->
