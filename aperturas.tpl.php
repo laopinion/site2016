7b6268276091b990002fd7135d4a50e0f0ccd0b8
John Andrey Serrano <andrey_201015@hotmail.com>
2016-08-26 17:56:56 -0500

    Terminando aperturas del home

---
+++ aperturas.tpl.php
@@ -0,0 +1,14 @@
+  <!--APERTURAS-->      
+  <div id="aperturas">
+    <!--Apertura One-->
+    <?php $view = views_get_view('aperturas'); print $view->preview('block'); ?>
+    <?php $view = views_get_view('aperturas'); print $view->preview('block_1'); ?>
+    <!--Fin apertura One-->
+
+    <!--Apertura Two-->
+    <?php //$view = views_get_view('aperturas'); print $view->preview('block_2'); ?>
+    <?php //$view = views_get_view('aperturas'); print $view->preview('block_3'); ?>
+    <?php //$view = views_get_view('aperturas'); print $view->preview('block_4'); ?>
+    <!--Fin apertura Two-->
+  </div>
+  <!--FIN APERTURAS-->
