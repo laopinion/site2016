0071a9df3dc2bc6c1e69817bc5dc5bd4ca5d0990
Jhonatan <Jhonatan_M@web-1.laopinion>
2016-08-26 18:12:09 -0500

    actualizando header

--- page--taxonomy--term--25.tpl.php
+++ page--taxonomy--term--25.tpl.php
@@ -1,3 +1,5 @@
+<div id="head_desktop"><?php include("header.tpl.php"); ?></div>
+<div id="head_movil"><?php include("header_movil.tpl.php"); ?></div>
 <section id="galerias">
     <div class="content">
     <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); print drupal_render(_block_get_renderable_array(_block_render_blocks(array($taxonomy_menu_block))));?><!--FIN seccion-->
