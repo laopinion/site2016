a1d6dec5ba20dee361b63c6da58989449f987e1d
John Andrey Serrano <andrey_201015@hotmail.com>
2016-09-03 11:27:57 -0500

    Modificaciones en la page de maintenance

--- maintenance-page.tpl.php
+++ maintenance-page.tpl.php
@@ -1,3 +1,5 @@
 <section id="maintenace">
-  <img src="<?php print base_path(); ?>sites/default/themes/op/images/Mantenimiento.jpg" alt="Mantenimiento">
+  <img style="margin: 0 auto;display: block;width: 50%;height: auto;" src="<?php print base_path(); ?>sites/default/themes/op/images/enconstruccion.gif" alt="Mantenimiento">
+
+  <img style="margin: 0 auto;display: block;width: 100%;height: auto;" src="<?php print base_path(); ?>sites/default/themes/op/images/mantenimiento.jpg" alt="Mantenimiento">
 </section>
