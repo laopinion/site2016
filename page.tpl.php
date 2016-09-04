570c99bb6d967098fe8dca63b196db81d3e353db
Jhonatan <Jhonatan_M@web-1.laopinion>
2016-09-04 16:14:07 -0500

    actualizando pautas

--- page.tpl.php
+++ page.tpl.php
@@ -5,7 +5,7 @@
 <?php if (!empty($tabs['#primary'])): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
 <!--FIN TABS -->
 <div id="container">
-<!--URGENTE--><?php $view = views_get_view('aperturas'); print $view->preview('block_8'); ?><!--FIN URGENTE-->
+<!--URGENTE--><?php // $view = views_get_view('aperturas'); print $view->preview('block_8'); ?><!--FIN URGENTE-->
   <section id="seccion">
     <div id="seccion_content">
       <div class="content"><?php print render($page['content']); ?></div>
