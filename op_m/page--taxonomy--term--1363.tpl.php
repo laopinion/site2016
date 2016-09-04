996d26ea2d98fec3d35d0d0837199108370089bc
John Andrey Serrano <andrey_201015@hotmail.com>
2016-09-04 18:40:22 -0500

    Cambiando view cache en las taxonomias

--- op_m/page--taxonomy--term--1363.tpl.php
+++ op_m/page--taxonomy--term--1363.tpl.php
@@ -7,19 +7,19 @@
     <div class="izq">
       <!--ANUNCIO 1000x90--><div class="pauta p1000x90" style="display: none"><?php include("pautas/S_1000x90.php"); ?></div><!--FIN ANUNCIO 1000x90-->
         <?php if (!empty($tabs['#primary'])): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); print drupal_render(_block_get_renderable_array(_block_render_blocks(array($taxonomy_menu_block))));?><!--FIN seccion-->
+        <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); $taxtmprender = _block_render_blocks(array($taxonomy_menu_block)); $taxtmprender2 = _block_get_renderable_array($taxtmprender); print drupal_render($taxtmprender2); ?><!--FIN seccion-->
          <?php include("compartir.tpl.php"); ?>
          <?php include("recomendada.tpl.php"); ?>
          <?php include("recomendada_movil.tpl.php"); ?>
          <div class="contenido">
-          <!--Editorial--><?php $view = views_get_view('opinion'); print $view->preview('block'); ?><!--FIN Editorial--> 
+          <!--Editorial--><?php print EjecutarViewCache('opinion','block','hora'); ?><!--FIN Editorial--> 
           <!--Caricaturas-->
           <div class="caricaturas"><div class="color"></div><h2>Caricaturas</h2>
             <div class="cari">
-              <!--Ultima caricatura--><?php $view = views_get_view('opinion'); print $view->preview('block_1'); ?><!--FIN Ultima caricatura-->
+              <!--Ultima caricatura--><?php print EjecutarViewCache('opinion','block_1','hora'); ?><!--FIN Ultima caricatura-->
               <!--Otras caricaturas-->
               <div class="otras">
-                <?php $view = views_get_view('opinion'); print $view->preview('block_2'); ?>
+                <?php print EjecutarViewCache('opinion','block_2','hora'); ?>
               </div><!--FIN Otras caricaturas-->
                 <div class="paginador">
                   <div class="item1 item">1</div>
@@ -27,16 +27,16 @@
                 </div>
             </div>
           </div><!--FIN Caricaturas-->
-          <!--Columnistas--><?php $view = views_get_view('opinion'); print $view->preview('block_3'); ?><!--FIN Columnistas-->
+          <!--Columnistas--><?php print EjecutarViewCache('opinion','block_3','hora'); ?><!--FIN Columnistas-->
          </div>
         </div><!--Fin izquierda-->
         <div class="der">
-          <!--Columnistas Destacado--><?php $view = views_get_view('opinion'); print $view->preview('block_4'); ?><!--FIN Columnistas Destacado-->
-          <?php $view = views_get_view('recientes'); print $view->preview('block'); ?><!--FIN Noticias Recientes-->
+          <!--Columnistas Destacado--><?php print EjecutarViewCache('opinion','block_4','hora'); ?><!--FIN Columnistas Destacado-->
+          <?php print EjecutarViewCache('recientes','block','corto'); ?><!--FIN Noticias Recientes-->
           <!--Juegos<div class="juegos"><?php include("juegos.tpl.php"); ?></div>-->
           <!--ANUNCIO 300x250--><div class="pauta p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php include("pautas/S_300x250_1.php"); ?></div></div><!--FIN ANUNCIO 300x250-->
           <!--Noticias Recientes-->
-          <!--Noticias Recientes--><?php $view = views_get_view('recientes'); print $view->preview('block_1'); ?><!--FIN Noticias Recientes-->   
+          <!--Noticias Recientes--><?php print EjecutarViewCache('recientes','block_1','corto'); ?><!--FIN Noticias Recientes-->   
           <!--Juegos--><div class="juegos"><a href="http://juegos.laopinion.com.co/"><img src="<?php print base_path(); ?>sites/default/themes/op/images/juegos.jpg"></a></div><!--FIN Juegos-->
         </div> <!--Fin derecha-->
 </section><!--Fin opinion-->
--- op_m/page--taxonomy--term--145.tpl.php
+++ op_m/page--taxonomy--term--145.tpl.php
@@ -7,43 +7,42 @@
       <div class="content">
       <!--ANUNCIO 1000x90--><div class="pauta p1000x90" style="display: none"><?php include("pautas/S_1000x90.php"); ?></div><!--FIN ANUNCIO 1000x90-->
         <?php if (!empty($tabs['#primary'])): ?><div id="tabs"><?php print render($tabs); ?></div><?php endif; ?>
-        
-        <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); print drupal_render(_block_get_renderable_array(_block_render_blocks(array($taxonomy_menu_block))));?><!--FIN seccion-->
+        <!--Seccion--><?php $taxonomy_menu_block = block_load('taxonomy_menu_block','1'); $taxtmprender = _block_render_blocks(array($taxonomy_menu_block)); $taxtmprender2 = _block_get_renderable_array($taxtmprender); print drupal_render($taxtmprender2); ?><!--FIN seccion-->
         <div class="izq">
-          <!--Destacada--><?php $view = views_get_view('s_general'); print $view->preview('block'); ?><!--FIN Destacada-->
-          <!--Lista--><?php $view = views_get_view('s_general'); print $view->preview('block_2'); ?><!--FIN Lista-->
+          <!--Destacada--><?php print EjecutarViewCache('s_general','block','corto'); ?><!--FIN Destacada-->
+          <!--Lista--><?php print EjecutarViewCache('s_general','block_2','mediano'); ?><!--FIN Lista-->
         </div>
         <div class="der movil">
-          <!--Derecha--><?php $view = views_get_view('s_general'); print $view->preview('block_1'); ?><!--FIN Derecha-->
+          <!--Derecha--><?php print EjecutarViewCache('s_general','block_1','mediano'); ?><!--FIN Derecha-->
          <!--ANUNCIO 300x250--><div class="pauta p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php include("pautas/S_300x250_1.php"); ?></div></div><!--FIN ANUNCIO 300x250-->
         </div> <!--Fin der-->
         <div class="mid">           
-          <!--Actividad Social--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_15'); ?><!--FIN Actividad Social-->
+          <!--Actividad Social--><?php print EjecutarViewCache('s_general_sub','block_15','mediano'); ?><!--FIN Actividad Social-->
         </div>
         <!--ANUNCIO 728x90--><div class="pauta desktop p728x90" style="margin: 0 135px 15px 135px;"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php include("pautas/S_728x90.php"); ?></div></div><!--FIN ANUNCIO 728x90-->
          <div class="mid" style="border-bottom: none;">           
-          <!--Tecnología--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_16'); ?><!--FIN Tecnología-->
+          <!--Tecnología--><?php print EjecutarViewCache('s_general_sub','block_16','mediano'); ?><!--FIN Tecnología-->
         </div>
         <div class="mid" style="border-bottom: none;">           
-          <!--Cultura--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_17'); ?><!--FIN Cultura-->
+          <!--Cultura--><?php print EjecutarViewCache('s_general_sub','block_17','mediano'); ?><!--FIN Cultura-->
         </div>
          <!--ANUNCIO 728x90--><div class="pauta desktop p728x90" style="margin: 0 135px 15px 135px;"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php include("pautas/S_728x90.php"); ?></div></div><!--FIN ANUNCIO 728x90-->
         <div class="mid" style="border-bottom: none;">           
-          <!--Entretenimiento--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_18'); ?><!--FIN Entretenimiento-->
+          <!--Entretenimiento--><?php print EjecutarViewCache('s_general_sub','block_18','mediano'); ?><!--FIN Entretenimiento-->
         </div>
         <div class="mid" style="border-bottom: none;">           
-          <!--Viral--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_19'); ?><!--FIN Viral-->
+          <!--Viral--><?php print EjecutarViewCache('s_general_sub','block_19','mediano'); ?><!--FIN Viral-->
         </div>
         <div class="mid">           
-          <!--Vida y salud--><?php $view = views_get_view('s_general_sub'); print $view->preview('block_20'); ?><!--FIN Vida y salud-->
+          <!--Vida y salud--><?php print EjecutarViewCache('s_general_sub','block_20','mediano'); ?><!--FIN Vida y salud-->
         </div>
         <div class="mid"><!--Seguir leyendo-->          
-          <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_5'); ?><!--FIN Centro-->
+          <!--Centro--><?php print EjecutarViewCache('s_general','block_5','mediano'); ?><!--FIN Centro-->
           <div class="divisor"></div>
           <!--ANUNCIO 300x250--><div class="pauta p300x250" style="float:left"><h2 class="anuncio_txt">Publicidad</h2><div class="info"><?php include("pautas/S_300x250_2.php"); ?></div></div><!--FIN ANUNCIO 300x250-->
         </div><!--Fin Seguir leyendo-->
         <div class="mid" style="border: none;"><!--Mas noticias-->            
-          <!--Centro--><?php $view = views_get_view('s_general'); print $view->preview('block_4'); ?><!--FIN Centro-->
+          <!--Centro--><?php print EjecutarViewCache('s_general','block_4','hora'); ?><!--FIN Centro-->
         </div><!--Mas noticias--> 
       </div><!-- Fin content--> 
 </section> <!-- /#seccion -->
