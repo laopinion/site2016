b16fc18c0bc865035108a87824fb7e2b94de859f
John Andrey Serrano <andrey_201015@hotmail.com>
2016-09-07 18:25:38 -0500

    Pauta vyv

---
+++ vyv.php
@@ -0,0 +1,7 @@
+<div id="vyv-pauta">
+	<div class="title">Información comercial</div>
+	<div class="triangulo"></div>
+	<div class="titulo"><a href="#">Es momento de adquirir vivienda nueva</a></div>
+	<div class="foto"><a href="#"><img src="<?php print base_path(); ?>sites/default/themes/op/images/familia.jpg" alt=""></a></div>
+	<div class="info">¿Aún no has decidido en qué proyecto invertir? Si quieres conocer la casa de tus sueños, esta es tu oportunidad.</div>
+</div>
