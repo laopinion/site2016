82ca4af5cb0b72f8a9e127869a825fe637a62928
Jhonatan <Jhonatan_M@web-1.laopinion>
2016-11-15 12:12:57 -0500

    actualizanod el template

--- pautas/intext_embimedia.php
+++ pautas/intext_embimedia.php
@@ -11,7 +11,7 @@
  */
     (function() {
       window["SMIntextSetUp"] = {
-        c:’texto’,
+        c:"texto",
         adPosition:1,    tag_desktop:'http%253A%252F%252Fpubads.g.doubleclick.net%252Fgampad%252Fads%253Fsz%253D600x338%2526iu%253D%252F40135427%252FPB_laopinion.com.co_video_outstream%2526impl%253Ds%2526gdfp_req%253D1%2526env%253Dvp%2526output%253Dvast%2526unviewed_position_start%253D1%2526url%253D%255Breferrer_url%255D%2526description_url%253D%255Bdescription_url%255D%2526correlator%253D%255Btimestamp%255D',        tag_mobile:'http%3A%2F%2Fpubads.g.doubleclick.net%2Fgampad%2Fads%3Fsz%3D600x338%26iu%3D%2F40135427%2FPB_laopinion_co_video_outstream_mobile%26impl%3Ds%26gdfp_req%3D1%26env%3Dvp%26output%3Dvast%26unviewed_position_start%3D1%26url%3D%5Breferrer_url%5D%26description_url%3D%5Bdescription_url%5D%26correlator%3D%5Btimestamp%5D',
         passback:'SMpassBack'
       };
