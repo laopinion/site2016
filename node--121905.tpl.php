691321996767cc7fc3b2e76459a5e084b75b5886
John serrano <andrey_201015@hotmail.com>
2017-05-12 15:32:50 -0500

    Modificaciones en los terminos y condiciones

---
+++ node--121905.tpl.php
@@ -0,0 +1,28 @@
+<section id="terminosCondiciones">
+  <link id="template-politica" href="<?php print base_path(); ?>sites/default/themes/op/politicaTratamiento.html" rel="import" /> 
+  <link id="template-cliente" href="<?php print base_path(); ?>sites/default/themes/op/avisoClientes.html" rel="import" /> 
+  <link id="template-proveedor" href="<?php print base_path(); ?>sites/default/themes/op/avisoProveedor.html" rel="import" /> 
+  <p><h2 class="titulo">TÉRMINOS Y CONDICIONES</h2></p>
+
+  <p>Gracias por visitar la página we de LA OPINIÓN  y por revisar los términos y condiciones de uso.</p>
+
+  <p><strong>TÉRMINOS Y CONDICIONES</strong></p>
+
+  <p>El portal www.laopinion.com.co es de propiedad de la Opinión S.A. El acceso, participación y uso del portal es gratuito y está regido por los términos y condiciones que se incluyen <a href="http://www.laopinion.com.co/terminos-y-condiciones-de-uso">Términos y condiciones de uso</a>, los cuales se entienden conocidos y aceptados por los (las) usuarios(as) del portal.</p>
+
+  <p><strong>POÍLITICA Y AVISO DE PRIVACIDAD</strong></p>
+
+  <p>De conformidad con lo dispuesto en la Ley estatutaria 1581 de 2012 y a su Decreto Reglamentario 1377 de 2013, <strong>La Opinión S.A.</strong> Pone a disposición su política para el tratamiento de datos personales, aviso de privacidad de clientes externos y aviso de privacidad de proveedores.</p>
+
+  <ul id="terminosLista">
+    <li class="one"><h2 class="titleOne">1. Política de tratamiento de los datos personales</h2>
+      <section id="terminosPolitica"> </section>
+    </li>
+    <li class="two"><h2 class="titleTwo">2. Aviso de prívacidad - Clientes Externos</h2>
+      <section id="avisoCliente"></section>
+    </li>
+    <li class="three"><h2 class="titleThree">3. Aviso de prívacidad - Proveedor</h2>
+      <section id="avisoProveedor"></section>
+    </li>
+  </ul>
+</section>
